95588100fc54596da0ea5bab424e03183c30b9eb
ChuoiUhuhu <168626994+ChuoiUhuhu0727@users.noreply.github.com>
2025-07-31 13:49:03 +0700

    Update titanic_eda_solutions.py

--- Chapter 3/data_eda/titanic_eda_solutions.py
+++ Chapter 3/data_eda/titanic_eda_solutions.py
@@ -10,27 +10,33 @@ titanic = pd.read_csv('titanic.csv')
 titanic.head()
 titanic['Age'].max()
 titanic['Age'].min()
-# Step 2: Create a new column "age_group" that bins the "Age" into decades (0–9, 10–19, etc.).
+
+# Create a new column "age_group" 
+# that bins the "Age" into decades (0–9, 10–19, etc.).
 titanic['Age_bin'] = pd.cut(titanic['Age'], 8)
 titanic['Age_bin'].unique()
-titanic
+
+# Create new column Family_size
 titanic['SibSp'].unique()
 titanic['family_size'] = titanic['SibSp'] + titanic['Parch'] + 1
 titanic['family_size'].unique()
-titanic.pivot_table('Survived', index=['Age_bin', 'Sex'])
-titanic.pivot_table('family_size', index=['Pclass', 'Sex'], aggfunc='mean')
+
+# survival rates of age and sex
+titanic.pivot_table('Survived', index=['Age_bin', 'Sex']) # survival rates among ange and sex
+# mean of family members of each class and sex
+titanic.pivot_table('family_size', index=['Pclass', 'Sex'], aggfunc='mean') 
+
 titanic[titanic['Age'].isnull()]
 titanic = titanic.query('Age.isnull() == False')
 titanic.count()
 quartile = np.percentile(titanic['Fare'], [25,50,75])
-mu = quartile[1]
-sig = 0.74 * (quartile[2] - quartile[0])
+mu = quartile[1] # median of the dataset
+sig = 0.74 * (quartile[2] - quartile[0]) # out-range values
 
 # filter out passengers whose "Fare" 
 # is more than 3 sigmas from the median.
-
 titanic = titanic.query('Fare < 3*@sig + @mu')
-titanic
+# Visualize
 titanic.pivot_table('Survived', ['Age_bin', 'Sex', 'Pclass']).plot()
 titanic.pivot_table('Survived', ['Sex', 'Pclass']).plot()
 titanic.pivot_table('Fare', index='Embarked', aggfunc='mean').plot()
